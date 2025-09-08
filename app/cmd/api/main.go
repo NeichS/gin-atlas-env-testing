172d012ac4050dd911923e658c294f6ca73bf090
Ignacio Sanchez <nachoagusss1@gmail.com>
2025-09-08 18:18:42 -0300

    feat: modularizacion facha

--- app/cmd/api/main.go
+++ app/cmd/api/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"entdemo/ent"
 	"entdemo/internal/infrastructure"
+	"fmt"
 	"os"
 
 	"github.com/gin-gonic/gin"
-)
+
+    _ "github.com/lib/pq"
+	)	
+
 
 func main() {
 
@@ -17,6 +22,22 @@ func main() {
 		})
 	})
 
-	router.POST("/user", infrastructure.HandleCreateUser)
+	host := os.Getenv("POSTGRES_HOST")
+	dbPort := os.Getenv("POSTGRES_PORT")
+	usr := os.Getenv("POSTGRES_USER")
+	dbName := os.Getenv("POSTGRES_DB")
+	dbPass := os.Getenv("POSTGRES_PASSWORD")
+
+	connstr := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s", host, dbPort, usr, dbName, dbPass)
+
+    client, err := ent.Open("postgres",connstr)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	handlers := infrastructure.NewHandler(client)
+	
+	router.POST("/user", handlers.HandleCreateUser)
 	router.Run("0.0.0.0:" + port) // listen and serve on 0.0.0.0:8080
 }
--- app/internal/infrastructure/handlers.go
+++ app/internal/infrastructure/handlers.go
@@ -1,11 +1,12 @@
 package infrastructure
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
+
+	"entdemo/ent"
 )
 
 type CreateUserDTO struct {
@@ -13,16 +14,30 @@ type CreateUserDTO struct {
 	Name string `form:"name" json:"name" binding:"required"`
 }
 
-func HandleCreateUser(c *gin.Context) {
+type Handlers struct {
+	entClient *ent.Client
+}
+
+func NewHandler(client *ent.Client) *Handlers {
+	return &Handlers{
+		entClient: client,
+	}
+}
+
+func (h *Handlers) HandleCreateUser(c *gin.Context) {
 	objA := CreateUserDTO{}
 	// This c.ShouldBind consumes c.Request.Body and it cannot be reused.
-	if err := c.ShouldBindBodyWith(&objA, binding.JSON); err == nil {
-		resp := "user created successfully"
-		c.JSON(http.StatusCreated, gin.H{"status": resp})
-	} else {
-        c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	if err := c.ShouldBindBodyWith(&objA, binding.JSON); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
-	fmt.Println(objA)
+	newUsr, err := h.entClient.User.Create().SetName(objA.Name).SetAge(objA.Age).Save(c)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"usr": newUsr})
 }
